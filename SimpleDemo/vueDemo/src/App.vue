6e4643b8f6bdbea15b3765f9fb9b8394dd392f89
王婷婷 <2372454518@qq.com>
2019-02-26 16:38:49 +0800

    持续更新中

---
+++ SimpleDemo/vueDemo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <virtualList :data=data></virtualList>
+  </div>
+</template>
+
+<script>
+import virtualList from './pages/virtualList/virtualList.vue'
+export default {
+  components:{
+    virtualList
+  },
+  name: 'App',
+  data: function() {
+    return {
+      data:  [],
+      itemHeight:40
+    }
+  },
+  mounted(){
+ 
+    for(let i = 0; i < 200; i++) {
+      this.data.push({
+        value: i,
+        index: i
+      })
+    }
+ 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
